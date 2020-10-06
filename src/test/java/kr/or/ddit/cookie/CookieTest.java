55492aabdcd57c28e908d1089ba16973afa09d08
KMY <miyeon03085@gmail.com>
2020-10-06 12:50:55 +0900

    쿠키 값 조회하기

--- src/test/java/kr/or/ddit/cookie/CookieTest.java
+++ src/test/java/kr/or/ddit/cookie/CookieTest.java
@@ -7,16 +7,30 @@ import org.junit.Test;
 public class CookieTest {
 
 	@Test
-	public void getCookieValueTest(String cookieName) {
+	public void getCookieValueTest() {
 		
 		/***Given : 주어진 상황 기술 ***/
 		CookieSplit cookieSplit = new CookieSplit();
 
 		/***When : 행위 ***/
-		String cookieValue = cookieSplit.getCookieValue(cookieName);
+		String cookieValue = cookieSplit.getCookieValue("TEST");
 		
 		/***Then : 결과 ***/
-		assertEquals(cookieValue, cookieValue); // 기대값, 실제값
+		assertEquals("T", cookieValue); // 기대값, 실제값
+		
+	}
+	
+	@Test
+	public void getCookieValueTeste() {
+		
+		/***Given : 주어진 상황 기술 ***/
+		CookieSplit cookieSplit = new CookieSplit();
+
+		/***When : 행위 ***/
+		String cookieValue = cookieSplit.getCookieValue("USERID2");
+		
+		/***Then : 결과 ***/
+		assertEquals("", cookieValue); // 기대값, 실제값
 		
 	}
 }
