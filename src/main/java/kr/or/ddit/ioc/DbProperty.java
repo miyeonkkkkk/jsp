5d0411b8249afbc5cd83bc2bef57ec640b7a08c6
KMY <miyeon03085@gmail.com>
2020-11-02 16:06:20 +0900

    실습  . property-placeholder

---
+++ src/main/java/kr/or/ddit/ioc/DbProperty.java
@@ -0,0 +1,37 @@
+package kr.or.ddit.ioc;
+
+public class DbProperty {
+	
+	private String driver;
+	private String url;
+	private String user;
+	private String pass;
+	
+	public String getDriver() {
+		return driver;
+	}
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+
+}
